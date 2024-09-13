8c2df5c47b1dc59b6c4eb908875d9ae864bbfa9d
Rusiru <rusiruf@cbctechsol.com>
2024-09-13 17:04:02 +0530

    Player Component Done

---
+++ src/components/Player.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useState } from 'react'
+
+function Player({ initialName, symbol }) {
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName)
+
+    function handleEditClick() {
+        setIsEditing((prevEditing) => !prevEditing);
+    }
+    function handleChange(e) {
+        setPlayerName(e.target.value)
+    }
+
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+    //let btnCaption = "Edit"
+
+    if (isEditing) {
+        editablePlayerName = <input type='text' required value={playerName} onChange={handleChange} />
+        //btnCaption = "Save"
+    }
+
+    return (
+        <>
+            <li>
+                <span className="player">
+                    {editablePlayerName}
+                    <span className="player-symbol">{symbol}</span>
+                </span>
+                <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+            </li>
+        </>
+    )
+}
+
+export default Player
