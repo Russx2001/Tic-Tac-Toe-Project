9dc1510c95e4180e6731089866fc6cf29aba168b
Rusiru <rusiruf@cbctechsol.com>
2024-09-28 00:55:10 +0530

    Switching players (active player) function added

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const initialGameBoard = [
     [null, null, null],
@@ -6,15 +6,29 @@ const initialGameBoard = [
     [null, null, null],
 ];
 
-function GameBoard() {
+function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+
+    const [gameBoard, setGameBoard] = useState(initialGameBoard)
+    //const [player1, setPlayer1] = useState(true)
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+            const updatedGameBoard = [...prevGameBoard.map((innerArray) => [...innerArray])]
+            updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+            return updatedGameBoard;
+        });
+
+        onSelectSquare();
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => (
-                <li key={rowIndex}>{row}
+            {gameBoard.map((row, rowIndex) => (
+                <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button>{playerSymbol}</button>
+                                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                             </li>
                         ))}
                     </ol>
