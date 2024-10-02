dd3d31e531cbc9ba9b342b1978c9f25835592063
Rusiru <rusiruf@cbctechsol.com>
2024-10-02 18:11:50 +0530

    Show winner's name when game over, Rematch button done

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function GameOver({ winner }) {
+function GameOver({ winner, restart }) {
     return (
         <div id="game-over">
             <h2>Game Over!</h2>
             {winner && <p>{winner} Won!</p>}
-            {!winner && <p>It's Draw!</p>}
+            {!winner && <p>It's a Draw!</p>}
             <p>
-                <button>Rematch!</button>
+                <button onClick={restart}>Rematch!</button>
             </p>
         </div>
     )
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { useState } from 'react'
 
-function Player({ initialName, symbol, isActive }) {
+function Player({ initialName, symbol, isActive, onChangeName }) {
 
     const [isEditing, setIsEditing] = useState(false);
     const [playerName, setPlayerName] = useState(initialName)
 
     function handleEditClick() {
         setIsEditing((prevEditing) => !prevEditing);
+
+        if (isEditing) {
+            onChangeName(symbol, playerName)
+        }
+
     }
     function handleChange(e) {
         setPlayerName(e.target.value)
